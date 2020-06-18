85ec4df8b468659548f1049cbbcefc4e37a0b79a
lakshmiprasanna3 <prasannakalahastri@gmail.com>
2020-06-18 20:35:02 +0530

    first commit

--- app/src/main/java/com/example/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -19,6 +20,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     Button button;
@@ -26,13 +31,15 @@ public class MainActivity extends AppCompatActivity {
     TextView result;
     TextView temperature;
     TextView feels_like;
+    TextView date;
+    TextView humidity;
+    ImageView img;
     //https://api.openweathermap.org/data/2.5/weather?q=Jaipur&appid=17fdb77a3aff08add476987521034308
 
 
     String baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
     String API = "&appid=17fdb77a3aff08add476987521034308";
-
 
 
     @Override
@@ -45,55 +52,77 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         temperature = findViewById(R.id.temperature);
         feels_like = findViewById(R.id.feelslike);
+        date = findViewById(R.id.date);
+        humidity = findViewById(R.id.hum);
+
+        img = findViewById(R.id.imageview);
 
 
-
-        if(city.getText().toString()== null){
-           city.setError("Please enter city name!!!");
+        if (city.getText().toString() == null) {
+            city.setError("Please enter city name!!!");
         }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String myURL = baseURL + city.getText().toString() + API;
-               // Log.i("URL","URL: "+ myURL);
+                // Log.i("URL","URL: "+ myURL);
 
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, myURL, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
                         try {
+
                             JSONObject mainobj = response.getJSONObject("main");
                             String temp = String.valueOf(mainobj.getDouble("temp"));
                             String info = response.getString("weather");
                             String feelslike = String.valueOf(mainobj.getDouble("feels_like"));
+                            String humi = String.valueOf(mainobj.getDouble("humidity"));
                             Log.i("INFO", "INFO: " + info);
                             JSONArray ar = response.getJSONArray("weather");
-                            for(int i=0 ; i < ar.length() ; i++){
+                            for (int i = 0; i < ar.length(); i++) {
                                 JSONObject parObj = ar.getJSONObject(i);
                                 String myWeather = parObj.getString("main");
                                 result.setText(myWeather);
 
-
+                                if (myWeather.equals("Mist")||myWeather.equals("Haze")){
+                                    img.setBackgroundResource(R.drawable.haze);
+                                }else if(myWeather.equals("Rain")){
+                                    img.setBackgroundResource(R.drawable.rain);
+                                }else if(myWeather.equals("Thunderstorm")){
+                                    img.setBackgroundResource(R.drawable.thunderstorm);
+                                }else if(myWeather.equals("Clear")){
+                                    img.setBackgroundResource(R.drawable.sun);
+                                }else if (myWeather.equals("Clouds")){
+                                    img.setBackgroundResource(R.drawable.cloud);
+                                }else{
+                                    img.setBackgroundResource(R.drawable.sun);
+                                }
 
 
                                 Log.i("ID", "ID: " + parObj.getString("id"));
                                 Log.i("MAIN", "MAIN: " + parObj.getString("main"));
                             }
-//
-                                  double cons = 273.15;
-                                double temp_cel = Double.parseDouble(temp);
-                                double centi = (temp_cel - cons);
+
+                            double cons = 274.15;
+                            double temp_cel = Double.parseDouble(temp);
+                            double centi = (temp_cel - cons);
                             centi = Math.round(centi);
-                             int j = (int)centi;
+                            int j = (int) centi;
                             double cons1 = 273.15;
                             double temp_cel1 = Double.parseDouble(feelslike);
                             double centi1 = (temp_cel1 - cons1);
                             centi1 = Math.round(centi1);
-                            int k = (int)centi1;
+                            int k = (int) centi1;
 
-                                temperature.setText(String.valueOf(j)+"°C");
-                                feels_like.setText("Feels Like "+String.valueOf(k)+"°C");
+                            temperature.setText(String.valueOf(j) + "°C");
+                            feels_like.setText("Feels Like " + String.valueOf(k) + "°C");
+                            Calendar calendar = Calendar.getInstance();
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                            String formatted_date = sdf.format(calendar.getTime());
+                            date.setText("Date: "+formatted_date);
+                            humidity.setText(humi+"%");
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -113,13 +142,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-
     }
-
-
-
-
 
 
 }
